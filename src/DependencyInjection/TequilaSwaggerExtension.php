17b3ffbfb899e7523fb7681fa8a1155410f227b9
Petr Buchyn <petrbuchyn@gmail.com>
2018-03-01 23:40:45 +0200

    Fixed services config

--- src/DependencyInjection/TequilaSwaggerExtension.php
+++ src/DependencyInjection/TequilaSwaggerExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Tequila\Bundle\SwaggerBundle\Generator\SwaggerGeneratorInterface;
 
 class TequilaSwaggerExtension extends ConfigurableExtension
 {
@@ -15,6 +16,10 @@ class TequilaSwaggerExtension extends ConfigurableExtension
             'tequila_swagger.path_patterns',
             $mergedConfig['allowed_path_patterns'] ?? []
         );
+
+        $container
+            ->registerForAutoconfiguration(SwaggerGeneratorInterface::class)
+            ->addTag('tequila.swagger.generator');
 
         $locator = new FileLocator(__DIR__.'/../Resources/config');
         $loader = new YamlFileLoader($container, $locator);
